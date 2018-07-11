d79c9dd15a64f34b85f08f0e45a79bc4cc598edc
michael rodsky <mrodsky@gmail.com>
2018-07-11 12:42:21 -0400

    established the Movie project with sln file, Client, Library, and Test projects

--- Movie.Client/Program.cs
+++ Movie.Client/Program.cs
@@ -4,6 +4,7 @@ namespace Movie.Client
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ Movie.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Movie.Library
+{
+    public class Class1
+    {
+    }
+}

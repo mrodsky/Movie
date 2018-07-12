9f92dc49bf2c45c7ac317978e8f534a3bf38b373
michael rodsky <mrodsky@gmail.com>
2018-07-12 13:33:27 -0400

    added EGenre Enum

---
+++ Movie.Library/Enums/EGenre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie.Library.Enums
+{
+   public enum EGenre
+    {
+        Action,
+        Anime,
+        Comedy,
+        Space_Western,
+        Romantic_Western,
+        Western
+    }
+}

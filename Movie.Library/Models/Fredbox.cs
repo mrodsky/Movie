2734bdac7c935944fc735a50660e24ccbde715fc
michael rodsky <mrodsky@gmail.com>
2018-07-12 13:34:14 -0400

    new model Frebox for movie distribution

---
+++ Movie.Library/Models/Fredbox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie.Library.Models
+{
+   public class Fredbox
+    {
+        private List<MovieClass> Movies;
+      
+        public Fredbox()
+        {
+            Movies = new List<MovieClass>();
+        }
+
+        public IEnumerable<MovieClass> GetMovies()
+        {
+            return Movies;
+        }
+
+        public MovieClass MakeMovie(string title)
+        {
+            return new MovieClass()
+            {
+                Title = title
+            };
+        }
+
+        public void DistributeMovie(MovieClass m)
+        {
+            Movies.Add(m);
+        }
+    }
+}
